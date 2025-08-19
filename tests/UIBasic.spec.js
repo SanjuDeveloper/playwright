f3c33af27ab568a9936a0f56eb37dc4a8bfbbeb6
SanjuDeveloper <bhattsanju41@gmai.com>
2025-08-19 19:33:27 +0530

    Promiss multiple window handle and notes added

--- tests/UIBasic.spec.js
+++ tests/UIBasic.spec.js
@@ -19,3 +19,29 @@ test.only("Page Fixture Test", async ({ page }) => {
     console.log(await page.locator("[style*='block']").textContent());
     expect(await page.locator("[style*='block']")).toContainText("Incorrect username/password");
 });
+
+test('@Child windows hadl', async ({browser})=>
+ {
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+    const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+ 
+    const [newPage]=await Promise.all(
+   [
+      context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+      documentLink.click(),
+   
+   ])//new page is opened
+   
+   // promiss tb use krte h jb hme multiple Asynchromously run krne hote h
+ 
+   const  text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@")
+    const domain =  arrayText[1].split(" ")[0]
+    //console.log(domain);
+    await page.locator("#username").fill(domain);
+    console.log(await page.locator("#username").valueInput());
+ 
+ })
